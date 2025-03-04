fecc49571df9e65ad8c936b2ebe8d9229a6321c4
Ghoulouis <maikhanh3241@gmail.com>
2025-03-04 11:26:30 +0700

    delete ess file

--- scripts/create_vault.ts
+++ scripts/create_vault.ts
@@ -2,7 +2,11 @@ import { PublicKey } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor"; // Import đầy đủ anchor
 import { Program } from "@coral-xyz/anchor";
 import { LuckyTrading } from "../target/types/lucky_trading";
-import { createAssociatedTokenAccount, getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import {
+    createAssociatedTokenAccount,
+    getAssociatedTokenAddressSync,
+    getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
 
 const VAULT_SEED = "vault";
 
@@ -13,7 +17,7 @@ async function main() {
     const program = anchor.workspace.LuckyTrading as Program<LuckyTrading>;
     const wallet = anchor.Wallet.local();
 
-    const agent = new PublicKey("Gg9UvaXUTwJvPXZN3rmjmBeGiU4FwQQf7q43aWH2uDMX");
+    const agent = new PublicKey("4xDfHSD5ac6sSSgNEUjU35tGnWuQCuzc9iUNNE4aNe1u");
     const collateral = new PublicKey("Bv773jeAs3nsU9NnUM8pYWAXsggqp2NCtpuMzSS3E1fg"); // USDC devnet
     const vaultPda = PublicKey.findProgramAddressSync(
         [Buffer.from(VAULT_SEED), agent.toBuffer()],
@@ -27,30 +31,24 @@ async function main() {
         console.log("Vault is already deployed at", vaultPda.toBase58());
     } catch {}
     console.log("isDeployed = ", false);
-    //if (isDeployed) return;
+    if (isDeployed) return;
 
-    const vaultCollatetalATA = await getOrCreateAssociatedTokenAccount(
-        provider.connection,
-        wallet.payer,
-        collateral,
-        vaultPda,
-        true
-    );
-    console.log("Vault collateral ATA:", vaultCollatetalATA.address.toBase58());
+    const vaultCollatetalATA = getAssociatedTokenAddressSync(collateral, vaultPda, true);
 
-    // const txHash = await program.methods
-    //     .openVault(agent)
-    //     .accountsPartial({
-    //         authority: wallet.publicKey,
-    //         collateral: collateral,
-    //         vault: vaultPda,
-    //     })
-    //     .signers([wallet.payer])
-    //     .rpc();
+    const txHash = await program.methods
+        .openVault(agent)
+        .accounts({
+            authority: wallet.publicKey,
+            collateral: collateral,
+            vault: vaultPda,
+            vaultCollateral: vaultCollatetalATA,
+        })
+        .signers([wallet.payer])
+        .rpc();
 
-    // console.log("Transaction hash:", txHash);
+    console.log("Transaction hash:", txHash);
 
-    // console.log(`View transaction on Solana Explorer: https://solscan.io/tx/${txHash}?cluster=devnet`);
+    console.log(`View transaction on Solana Explorer: https://solscan.io/tx/${txHash}?cluster=devnet`);
 }
 
 main().catch((err) => {
